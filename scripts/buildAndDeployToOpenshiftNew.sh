a125dd37e91a5b7585f0bf8f83370bdc7c59cf9c
jqiwen <jqiwen00@gmail.com>
2025-11-04 15:26:56 -0500

    750 update

--- scripts/buildAndDeployToOpenshiftNew.sh
+++ scripts/buildAndDeployToOpenshiftNew.sh
@@ -12,20 +12,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-MANIFESTS=manifests-openshift
-
-if [[ ${3} == "" ]]
-then
-  echo "Usage: buildAndDeployToOpenshift.sh  default-route/project_name internal-route/project_name route_host [podman]"
-  exit
+GROUP=11
+if [ "$GROUP" -eq 0 ]; then
+    echo "Error: please update your group number!"
+    exit 1  
 fi
+if [ $(oc project -q) != "acmeair-group${GROUP}" ]; then 
+  echo "Error: please update your current project/namespace!"
+  exit 1
+fi
+MANIFESTS=manifests-openshift
+DOCKERFILE=Dockerfile
 
-IMAGE_PREFIX_EXTERNAL=${1}
-IMAGE_PREFIX=${2}
-ROUTE_HOST=${3}
+EXTERNAL_REGISTRY=$(oc get route image-registry -n openshift-image-registry --template='{{ .spec.host }}')
+PROJECT_NAME=$(oc project -q)
 
-if [[ ${4} == "" ]]
+if [[ ${1} == "" ]]
 then
   echo "Using Docker to build/push"
   BUILD_TOOL="docker"
@@ -35,16 +37,33 @@ else
   TLS_VERIFY="--tls-verify=false"
 fi
 
+echo "Trying ${BUILD_TOOL} login -u $(oc whoami) -p $(oc whoami -t) ${TLS_VERIFY}  ${EXTERNAL_REGISTRY}"
+${BUILD_TOOL} login -u $(oc whoami) -p $(oc whoami -t) ${TLS_VERIFY}  ${EXTERNAL_REGISTRY}
+
+if [[ $? -ne 0 ]]
+then
+  echo "Login Failed" 
+  exit
+fi
+
+IMAGE_PREFIX_EXTERNAL=${EXTERNAL_REGISTRY}/${PROJECT_NAME}
+IMAGE_PREFIX=image-registry.openshift-image-registry.svc:5000/${PROJECT_NAME}
+ROUTE_HOST=acmeair${GROUP}$(echo ${EXTERNAL_REGISTRY} | awk '{gsub("default-route-openshift-image-registry","");print $1}')
 
 echo "Image Prefix External=${IMAGE_PREFIX_EXTERNAL}"
 echo "Image Prefix Internal=${IMAGE_PREFIX}"
 echo "Route Host=${ROUTE_HOST}"
 
+echo "Setting up Mongo"
+${BUILD_TOOL} pull --platform linux/amd64 docker.io/library/mongo:latest
+${BUILD_TOOL} tag docker.io/library/mongo:latest ${IMAGE_PREFIX_EXTERNAL}/mongo:latest
+${BUILD_TOOL} push ${IMAGE_PREFIX_EXTERNAL}/mongo:latest
+
 cd "$(dirname "$0")"
 cd ..
 kubectl delete -f ${MANIFESTS}
 mvn clean package
-${BUILD_TOOL} build --pull -t ${IMAGE_PREFIX_EXTERNAL}/acmeair-mainservice-java:latest --no-cache -f Dockerfile .
+${BUILD_TOOL} build --platform linux/amd64 --pull -t ${IMAGE_PREFIX_EXTERNAL}/acmeair-mainservice-java:latest --no-cache -f ${DOCKERFILE} .
 ${BUILD_TOOL} push ${IMAGE_PREFIX_EXTERNAL}/acmeair-mainservice-java:latest ${TLS_VERIFY}
 
 if [[ `grep -c ${IMAGE_PREFIX} ${MANIFESTS}/deploy-acmeair-mainservice-java.yaml` == 0 ]]
@@ -73,7 +92,7 @@ rm ${MANIFESTS}/deploy-acmeair-mainservice-java.yaml.bak
 cd ../acmeair-authservice-java
 kubectl delete -f ${MANIFESTS}
 mvn clean package
-${BUILD_TOOL} build --pull -t ${IMAGE_PREFIX_EXTERNAL}/acmeair-authservice-java --no-cache -f Dockerfile .
+${BUILD_TOOL} build --platform linux/amd64 --pull -t ${IMAGE_PREFIX_EXTERNAL}/acmeair-authservice-java --no-cache -f ${DOCKERFILE} .
 ${BUILD_TOOL} push ${IMAGE_PREFIX_EXTERNAL}/acmeair-authservice-java:latest ${TLS_VERIFY} 
 
 if [[ `grep -c ${IMAGE_PREFIX} ${MANIFESTS}/deploy-acmeair-authservice-java.yaml` == 0 ]]
@@ -102,13 +121,14 @@ rm ${MANIFESTS}/deploy-acmeair-authservice-java.yaml.bak
 cd ../acmeair-bookingservice-java
 kubectl delete -f ${MANIFESTS}
 mvn clean package
-${BUILD_TOOL} build --pull -t ${IMAGE_PREFIX_EXTERNAL}/acmeair-bookingservice-java --no-cache  -f Dockerfile .
+${BUILD_TOOL} build --platform linux/amd64 --pull -t ${IMAGE_PREFIX_EXTERNAL}/acmeair-bookingservice-java --no-cache -f ${DOCKERFILE} .
 ${BUILD_TOOL} push ${IMAGE_PREFIX_EXTERNAL}/acmeair-bookingservice-java:latest ${TLS_VERIFY} 
 
 if [[ `grep -c ${IMAGE_PREFIX}/a ${MANIFESTS}/deploy-acmeair-bookingservice-java.yaml` == 0 ]]
 then
   echo "Adding ${IMAGE_PREFIX}/"
   sed -i.bak "s@acmeair-bookingservice-java:latest@${IMAGE_PREFIX}/acmeair-bookingservice-java:latest@" ${MANIFESTS}/deploy-acmeair-bookingservice-java.yaml
+  sed -i.bak "s@docker.io/library/mongo:latest@${IMAGE_PREFIX}/mongo:latest@" ${MANIFESTS}/deploy-acmeair-bookingservice-java.yaml
 fi
 
 if [[ `grep -c ${ROUTE_HOST} ${MANIFESTS}/acmeair-bookingservice-route.yaml` == 0 ]]
@@ -121,9 +141,11 @@ kubectl apply -f ${MANIFESTS}
 
 echo "Removing ${IMAGE_PREFIX}"
 sed -i.bak "s@${IMAGE_PREFIX}/acmeair-bookingservice-java:latest@acmeair-bookingservice-java:latest@" ${MANIFESTS}/deploy-acmeair-bookingservice-java.yaml
+sed -i.bak "s@${IMAGE_PREFIX}/mongo:latest@docker.io/library/mongo:latest@" ${MANIFESTS}/deploy-acmeair-bookingservice-java.yaml
 
 echo "Removing ${ROUTE_HOST}"
 sed -i.bak "s@${ROUTE_HOST}@_HOST_@" ${MANIFESTS}/acmeair-bookingservice-route.yaml
+
 
 rm ${MANIFESTS}/acmeair-bookingservice-route.yaml.bak
 rm ${MANIFESTS}/deploy-acmeair-bookingservice-java.yaml.bak
@@ -133,13 +155,14 @@ rm ${MANIFESTS}/deploy-acmeair-bookingservice-java.yaml.bak
 cd ../acmeair-customerservice-java
 kubectl delete -f ${MANIFESTS}
 mvn clean package
-${BUILD_TOOL} build --pull -t ${IMAGE_PREFIX_EXTERNAL}/acmeair-customerservice-java --no-cache -f Dockerfile .
+${BUILD_TOOL} build --platform linux/amd64 --pull -t ${IMAGE_PREFIX_EXTERNAL}/acmeair-customerservice-java --no-cache -f ${DOCKERFILE} .
 ${BUILD_TOOL} push ${IMAGE_PREFIX_EXTERNAL}/acmeair-customerservice-java:latest ${TLS_VERIFY} 
 
 if [[ `grep -c ${IMAGE_PREFIX}/a ${MANIFESTS}/deploy-acmeair-customerservice-java.yaml` == 0 ]]
 then
   echo "Adding ${IMAGE_PREFIX}/"
   sed -i.bak "s@acmeair-customerservice-java:latest@${IMAGE_PREFIX}/acmeair-customerservice-java:latest@" ${MANIFESTS}/deploy-acmeair-customerservice-java.yaml
+  sed -i.bak "s@docker.io/library/mongo:latest@${IMAGE_PREFIX}/mongo:latest@" ${MANIFESTS}/deploy-acmeair-customerservice-java.yaml
 fi
 
 if [[ `grep -c ${ROUTE_HOST} ${MANIFESTS}/acmeair-customerservice-route.yaml` == 0 ]]
@@ -152,6 +175,7 @@ kubectl apply -f ${MANIFESTS}
 
 echo "Removing ${IMAGE_PREFIX}"
 sed -i.bak "s@${IMAGE_PREFIX}/acmeair-customerservice-java:latest@acmeair-customerservice-java:latest@" ${MANIFESTS}/deploy-acmeair-customerservice-java.yaml
+sed -i.bak "s@${IMAGE_PREFIX}/mongo:latest@docker.io/library/mongo:latest@" ${MANIFESTS}/deploy-acmeair-customerservice-java.yaml
 
 echo "Removing ${ROUTE_HOST}"
 sed -i.bak "s@${ROUTE_HOST}@_HOST_@" ${MANIFESTS}/acmeair-customerservice-route.yaml
@@ -162,13 +186,14 @@ rm ${MANIFESTS}/deploy-acmeair-customerservice-java.yaml.bak
 cd ../acmeair-flightservice-java
 kubectl delete -f ${MANIFESTS}
 mvn clean package
-${BUILD_TOOL} build --pull -t ${IMAGE_PREFIX_EXTERNAL}/acmeair-flightservice-java --no-cache -f Dockerfile .
+${BUILD_TOOL} build --platform linux/amd64 --pull -t ${IMAGE_PREFIX_EXTERNAL}/acmeair-flightservice-java --no-cache -f ${DOCKERFILE} .
 ${BUILD_TOOL} push ${IMAGE_PREFIX_EXTERNAL}/acmeair-flightservice-java:latest ${TLS_VERIFY}
 
 if [[ `grep -c ${IMAGE_PREFIX}/a ${MANIFESTS}/deploy-acmeair-flightservice-java.yaml` == 0 ]]
 then
   echo "Adding ${IMAGE_PREFIX}/"
   sed -i.bak "s@acmeair-flightservice-java:latest@${IMAGE_PREFIX}/acmeair-flightservice-java:latest@" ${MANIFESTS}/deploy-acmeair-flightservice-java.yaml
+  sed -i.bak "s@docker.io/library/mongo:latest@${IMAGE_PREFIX}/mongo:latest@" ${MANIFESTS}/deploy-acmeair-flightservice-java.yaml
 fi
 
 if [[ `grep -c ${ROUTE_HOST} ${MANIFESTS}/acmeair-flightservice-route.yaml` == 0 ]]
@@ -181,12 +206,10 @@ kubectl apply -f ${MANIFESTS}
 
 echo "Removing ${IMAGE_PREFIX}"
 sed -i.bak "s@${IMAGE_PREFIX}/acmeair-flightservice-java:latest@acmeair-flightservice-java:latest@" ${MANIFESTS}/deploy-acmeair-flightservice-java.yaml
+sed -i.bak "s@${IMAGE_PREFIX}/mongo:latest@docker.io/library/mongo:latest@" ${MANIFESTS}/deploy-acmeair-flightservice-java.yaml
 
 echo "Removing ${ROUTE_HOST}"
 sed -i.bak "s@${ROUTE_HOST}@_HOST_@" ${MANIFESTS}/acmeair-flightservice-route.yaml
 
 rm ${MANIFESTS}/acmeair-flightservice-route.yaml.bak
 rm ${MANIFESTS}/deploy-acmeair-flightservice-java.yaml.bak
-
-
-
